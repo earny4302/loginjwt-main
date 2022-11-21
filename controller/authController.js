216f5dc6fb48f8de4f721499296d45a9370ca459
earny4302 <73746161+earny4302@users.noreply.github.com>
2022-11-21 23:48:35 +0530

    Initial commit

---
+++ controller/authController.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('../config');
+const User = require('../model/userModel');
+
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(bodyParser.json());
+
+//Get All Users
+router.get('/users',(req,res) => {
+    User.find({},(err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+//register users
+router.post('/register', (req,res) => {
+    //encrypt password
+    let hashPassword = bcrypt.hashSync(req.body.password, 8);
+    //8 length of password
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashPassword,
+        phone:req.body.phone,
+        role:req.body.role?req.body.role:'User'
+    },(err,data) => {
+        if(err) return res.send('Erroe While Register')
+        res.send('Registration Successfull')
+    })
+})
+
+
+//login User
+router.post('/login',(req,res) => {
+    User.findOne({email:req.body.email},(err,user) => {
+        if(err) return res.send({auth:false,token:'Error While Logging'});
+        if(!user) return res.send({auth:false,token:'No User Found'})
+        else{
+            const passIsValid = bcrypt.compareSync(req.body.password,user.password);
+            if(!passIsValid) return res.send({auth:false,token:'Invalid Password'})
+            // in case both match 
+            let token = jwt.sign({id:user._id},config.secret,{expiresIn:86400}) //24 hours
+            res.send({auth:true,token:token})
+        }
+    })
+})
+
+//userInfo
+router.get('/userInfo',(req,res) => {
+    let token = req.headers['x-access-token'];
+    if(!token) res.send({auth:false,token:'No Token Provided'})
+    // jwt verify
+    jwt.verify(token,config.secret,(err,user) => {
+        if(err) return res.send({auth:false,token:'Invalid Token'})
+        User.findById(user.id,(err,result) =>{
+            res.send(result)
+        })
+    })
+})
+
+//delete user
+router.delete('/delete',(req,res) =>{
+    User.remove({},(err,data) => {
+        if(err) throw err;
+        res.send("User Deleted")
+    })
+})
+
+module.exports = router;
---
+++ db.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+//mongoose.connect("mongodb+srv://trial321:trial321@atlascluster.kpsc2.mongodb.net/Zdata?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://earny4302:earny4302@cluster0.ttnfdmp.mongodb.net/GMH?retryWrites=true&w=majority")
